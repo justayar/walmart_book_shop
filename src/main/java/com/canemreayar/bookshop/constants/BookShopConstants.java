0e85e57c0239541485273e68528b0edb1a39531a
can.ayar <Can.Ayar1@vodafone.com>
2020-01-27 12:12:16 +0300

    Assets url to application properties

--- src/main/java/com/canemreayar/bookshop/constants/BookShopConstants.java
+++ src/main/java/com/canemreayar/bookshop/constants/BookShopConstants.java
@@ -10,8 +10,6 @@ public class BookShopConstants {
     public static final int NUMBER_OF_REVIEWS_PER_PAGE = 5;
     public static final String API_FORMAT = "json";
     public static final int BOOK_CATEGORY_ID = 3920;
-    public static final String ASSETS_URL = "http://localhost:8080";
-    public static final int DEFAULT_OPENED_PAGE_NUM = 0;
     public static final String ONLINE_AVAILABLE = "Available";
     public static final String ONLINE_NOT_AVAILABLE = "Not Available";
     public static final String OFFER_TYPE_ONLY_ONLINE = "ONLINE_ONLY";
--- src/main/java/com/canemreayar/bookshop/util/ApplicationUtils.java
+++ src/main/java/com/canemreayar/bookshop/util/ApplicationUtils.java
@@ -1,6 +1,6 @@
 package com.canemreayar.bookshop.util;
 
-import com.canemreayar.bookshop.constants.BookShopConstants;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -8,13 +8,17 @@ import org.springframework.web.servlet.ModelAndView;
 @Component
 public class ApplicationUtils {
 
+
+    @Value("${assets.url}")
+    private String ASSETS_URL;
+
     private ApplicationUtils(){ }
 
     public ModelAndView getModelAndView(String viewName,String modelName,Object modelObject){
 
         ModelAndView modelAndView = new ModelAndView(viewName,modelName,modelObject);
 
-        modelAndView.addObject("assetsUrl", BookShopConstants.ASSETS_URL);
+        modelAndView.addObject("assetsUrl",ASSETS_URL);
 
         return modelAndView;
 
